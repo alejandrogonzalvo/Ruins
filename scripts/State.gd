5d7872b9baf2e2867864729a00fe54c039869658
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2023-08-16 12:21:00 +0200

    add Entity class

--- scripts/State.gd
+++ scripts/State.gd
@@ -1,10 +1,14 @@
 extends Node2D
 class_name State
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var enter_action
+var process_action
+var exit_action
 
+func _init(enter_action, process_action, exit_action):
+	self.enter_action = enter_action
+	self.process_action = process_action
+	self.exit_action = exit_action
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- scripts/StateMachine.gd
+++ scripts/StateMachine.gd
@@ -2,15 +2,16 @@ extends Node2D
 class_name StateMachine
 
 var current_state: State
-var transitions: Array
+var transitions: Array[Transition]
 
 func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	current_state.process_action.call()
+	
 	for transition in transitions:
-		(transition as Transition)
 		if transition.initial_state != current_state:
 			continue
 		
@@ -18,5 +19,13 @@ func _process(delta):
 			change_state(transition.final_state)
 
 
-func change_state(state: State):
+func change_state(state: State) -> void:
+	current_state.exit_action.call()
 	current_state = state
+	current_state.enter_action.call()
+
+func add_state() -> void:
+	pass
+	
+func add_transition(initial_state: State, final_state: State, transition_function) -> void :
+	transitions.append(Transition.new(initial_state, final_state, transition_function))
